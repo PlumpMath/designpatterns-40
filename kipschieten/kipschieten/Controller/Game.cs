3c24e308d5a79ac9d6622e227956690a6f4df71f
Alex Nagelkerke <alex_na@outlook.com>
2014-09-03 14:26:33 +0200

    Initial commit

---
+++ kipschieten/kipschieten/Controller/Game.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace kipschieten.Controller
+{
+    class Game
+    {
+        private Boolean _running = false;
+        private MainWindow _mainWindow;
+        private DateTime _epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        public Game()
+        {
+            _mainWindow = new MainWindow();
+
+
+            _mainWindow.Show();
+            Run();
+        }
+
+
+
+        public void Run()
+        {
+            long beforeTime, timeDiff, sleepTime, frameTime;
+            frameTime = 1 / 60;
+
+            beforeTime = (long)(DateTime.UtcNow - _epoch).TotalMilliseconds;
+            _running = true;
+            while (_running)
+            {
+
+
+
+                timeDiff = (long)((DateTime.UtcNow - _epoch).TotalMilliseconds - beforeTime);
+                sleepTime = frameTime - timeDiff;
+
+                if (sleepTime <= 0)
+                    sleepTime = 5;
+
+                try
+                {
+                    Thread.Sleep(new TimeSpan(0,0,0,0,(int)sleepTime));
+                }
+                catch (ThreadInterruptedException e) { }
+
+                beforeTime = (long)(DateTime.UtcNow - _epoch).TotalMilliseconds;
+            }
+        }
+
+        public void GameOver()
+        {
+
+        }
+    }
+}
---
+++ kipschieten/kipschieten/Model/Manager.cs
@@ -0,0 +1,25 @@
+﻿using kipschieten.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace kipschieten.Model
+{
+    class Manager
+    {
+        private Player _player;
+        private List<Chicken> _chickens;
+
+        public Manager()
+        {
+        }
+
+        public void Update()
+        {
+
+        }
+    }
+}
